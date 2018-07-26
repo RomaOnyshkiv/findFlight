859a39d9f8e3b6f96be4d0b1d70539185fe415f8
RomaOnyshkiv <romaonyshkib@gmail.com>
2018-07-26 15:29:49 +0200

    simple search added

--- src/main/java/org/searchFlight/DTO/ScyscannerSearchDTO.java
+++ src/main/java/org/searchFlight/DTO/ScyscannerSearchDTO.java
@@ -1,4 +1,68 @@
 package org.searchFlight.DTO;
 
 public class ScyscannerSearchDTO {
+
+    private String country;
+    private String currency;
+    private String locale;
+    private String originalPlace;
+    private String destinationPlace;
+    private String outboundPartialDate;
+    private String inboundPartialDate;
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    public String getLocale() {
+        return locale;
+    }
+
+    public void setLocale(String locale) {
+        this.locale = locale;
+    }
+
+    public String getOriginalPlace() {
+        return originalPlace;
+    }
+
+    public void setOriginalPlace(String originalPlace) {
+        this.originalPlace = originalPlace;
+    }
+
+    public String getDestinationPlace() {
+        return destinationPlace;
+    }
+
+    public void setDestinationPlace(String destinationPlace) {
+        this.destinationPlace = destinationPlace;
+    }
+
+    public String getOutboundPartialDate() {
+        return outboundPartialDate;
+    }
+
+    public void setOutboundPartialDate(String outboundPartialDate) {
+        this.outboundPartialDate = outboundPartialDate;
+    }
+
+    public String getInboundPartialDate() {
+        return inboundPartialDate;
+    }
+
+    public void setInboundPartialDate(String inboundPartialDate) {
+        this.inboundPartialDate = inboundPartialDate;
+    }
 }
--- src/main/java/org/searchFlight/controllers/SkyscannerController.java
+++ src/main/java/org/searchFlight/controllers/SkyscannerController.java
@@ -4,15 +4,46 @@ import org.searchFlight.DTO.ScyscannerSearchDTO;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import static org.springframework.web.bind.annotation.RequestMethod.*;
 
 @RestController
 public class SkyscannerController {
 
-    @RequestMapping(value = "/find", method = RequestMethod.POST)
+    private final String BASE_URL = "http://partners.api.skyscanner.net/apiservices";
+    private final String API_KEY = "?apikey=prtl6749387986743898559646983194";
+    private String country;
+    private String currency;
+    private String locale;
+    private String originalPlace;
+    private String destinationPlace;
+    private String outboundPartialDate;
+    private String inboundPartialDate;
+
+    @RequestMapping(value = "/find", method = POST)
     public ResponseEntity findFlight(@RequestBody ScyscannerSearchDTO scyscannerSearchDTO) {
 
         return null;
+    }
+
+    @RequestMapping(value = "/", method = GET)
+    public String index() {
+        return "Home Page";
+    }
+
+    @RequestMapping(value = "/test", method = POST)
+    public ResponseEntity<String> test(@RequestBody ScyscannerSearchDTO scyscannerSearchDTO) {
+        country = scyscannerSearchDTO.getCountry();
+        currency = scyscannerSearchDTO.getCurrency();
+        locale = scyscannerSearchDTO.getLocale();
+        originalPlace = scyscannerSearchDTO.getOriginalPlace();
+        destinationPlace = scyscannerSearchDTO.getDestinationPlace();
+        outboundPartialDate = scyscannerSearchDTO.getOutboundPartialDate();
+        inboundPartialDate = scyscannerSearchDTO.getInboundPartialDate();
+        String endpoint = country + "/" + currency+ "/" + locale + "/" + originalPlace + "/" + destinationPlace + "/" + outboundPartialDate + "/" + inboundPartialDate + API_KEY;
+        RestTemplate template = new RestTemplate();
+        return template.getForEntity(BASE_URL + "/browsequotes/v1.0/" + endpoint, String.class);
     }
 }
